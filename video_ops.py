fd4ef9ec75a6f395b0bd0c8919a866c369c930f3
nikhil.ketkar <nikhil.s.ketkar@gmail.com>
2018-10-07 09:25:52 +0530

    Trying to build a video from images

--- video_ops.py
+++ video_ops.py
@@ -6,7 +6,6 @@ def process_meta(meta_path):
     df = pandas.read_csv(meta_path)
     df.columns = ["timestamp_ms", "class_name", "object_id", "object_presence", "xmin", "xmax", "ymin", "ymax"]
     df["timestamp"] = df["timestamp_ms"]/1000.0
-    df = df[df["object_presence"] == "present"]
     result = []
     for name, group in df.groupby(["timestamp"]):
         curr_result = []
@@ -27,10 +26,26 @@ def index_video(video_path, meta_path, images_path):
         image = cv2.imread("/tmp/temp.png")
 
         for frame_object in frame_objects:
-            xmin = int(frame_object["xmin"] * image_x)
-            xmax = int(frame_object["xmin"] * image_x)
-            ymin = int(frame_object["ymax"] * image_y)
-            ymax = int(frame_object["ymax"] * image_y)
-            cv2.rectangle(image, (xmin, xmax), (ymin, ymax), (0,255,0), 4)
+            if frame_object["object_presence"] == "present":
+                xmin = int(frame_object["xmin"] * image_x)
+                xmax = int(frame_object["xmin"] * image_x)
+                ymin = int(frame_object["ymax"] * image_y)
+                ymax = int(frame_object["ymax"] * image_y)
+                cv2.rectangle(image, (xmin, xmax), (ymin, ymax), (0,255,0), 4)
+                cv2.putText(image,
+                           "(" + frame_object["class_name"] + "," + str(frame_object["object_id"]) + ")",
+                           (xmin, ymin), cv2.FONT_HERSHEY_SIMPLEX,0.8, (0, 255, 0), 2, cv2.LINE_AA)
 
         cv2.imwrite(images_path + "/" + str(int(timestamp)) + ".png", image)
+
+def build_video(images_path, min_value, max_value, video_path):
+    images_list = []
+    for i in range(min_value, max_value + 1):
+        images_list.append(images_path + "/" + str(i) + ".png")
+    clip = moviepy.editor.ImageSequenceClip(images_list, fps=30)
+    clip.write_videofile(video_path + ".mp4")
+
+    
+    
+    
+    
